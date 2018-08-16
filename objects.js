85e93e399a8c4885da090c7f0d3020fa333caed7
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-16 19:46:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artist: "song" }
 
-function updatePlayList(playlist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
