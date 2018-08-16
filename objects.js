8b6be5a5f192248ac9b3b05209a000be7d0024d3
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-16 19:47:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { artist: "song" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+  delete playlist[artistName]
   return playlist
 }
 
